9a1949a38aa81d04337b3b19733d1d7af9cf53af
SungHwan <tjdghks130907@gmail.com>
2023-08-05 01:10:08 +0900

    [feature/view-login] 인증완료 되어 index 페이지 접근 시 회원의 이름을 model에 담아 전달하도록 추가

--- src/main/java/com/wanted/controller/HomeController.java
+++ src/main/java/com/wanted/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.wanted.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Authentication authentication) {
+    public String home(Authentication authentication, Model model) {
         log.debug("[home page]");
         // 인증이 완료된 경우
         if (authentication != null) {
             log.debug("{} 님이 메인페이지로 접근하였습니다.", authentication.getName());
+            model.addAttribute("username", authentication.getName());
             return "index";
         }
         // 인증이 완료되지 않은 경우 - 로그인 페이지
