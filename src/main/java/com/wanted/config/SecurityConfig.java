aee2b904633e43b3a04a1a6ed1099a1786a6c6cb
SungHwan <tjdghks130907@gmail.com>
2023-08-04 16:42:59 +0900

    [feature/board-add] 스프링 시큐리티 설정 변경 - 게시글 관련 url 요청은 모두 권한 체크

--- src/main/java/com/wanted/config/SecurityConfig.java
+++ src/main/java/com/wanted/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .httpBasic(httpBasic -> httpBasic.disable())    // bearer 방식을 사용, http basic 방식은 비활성화
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // jwt 방식이므로 세션 사용안하도록 설정
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/").authenticated()    // home은 권한이 있어야 접근 가능
+                        request.requestMatchers("/board/**").authenticated()    // 게시글 관련 url 은 모두 권한 필요
                                 .anyRequest().permitAll()   // 나머지 요청에 대해서는 모두 허용
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtKey, jwtProvider), UsernamePasswordAuthenticationFilter.class)
