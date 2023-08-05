c6ad8c4db6eb3e4dbc9237549e80a1fc587093df
SungHwan <tjdghks130907@gmail.com>
2023-08-05 17:51:31 +0900

    [feature/board-lookup] 게시글 조회, 게시글 조회 수 증가 기능 추가

--- src/main/java/com/wanted/repository/BoardMapper.java
+++ src/main/java/com/wanted/repository/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.wanted.repository;
 
 import com.wanted.domain.Board;
+import com.wanted.domain.BoardLookupInfo;
 import com.wanted.domain.BoardViewInfo;
 import com.wanted.domain.PageMakeVO;
 import org.apache.ibatis.annotations.Mapper;
@@ -15,9 +16,11 @@ public interface BoardMapper {
     // 게시글 등록
     void save(@Param("board") Board board, @Param("memberNo") Long memberNo);
     // 게시글 조회
-    Optional<Board> findById(Long boardId);
+    Optional<BoardLookupInfo> findById(Long boardId);
     // 게시글 목록
     List<BoardViewInfo> findAll(@Param("paging") PageMakeVO pageMakeVO);
     // 전체 게시글 수
     int total();
+    // 게시글 조회 수 증가
+    void updateLookupCnt(Long boardId);
 }
