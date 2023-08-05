ddaea273aa1e85dec90ca4078282244c758c4817
SungHwan <tjdghks130907@gmail.com>
2023-08-05 22:31:08 +0900

    [feature/board-delete] 게시글 삭제 처리 구현 ( 게시글 작성자만 삭제 가능하도록 처리 )

--- src/main/java/com/wanted/controller/api/BoardApiController.java
+++ src/main/java/com/wanted/controller/api/BoardApiController.java
@@ -32,7 +32,7 @@ public class BoardApiController {
         log.debug("{} 님이 게시글 등록을 요청합니다. boardAdd = {}", authentication.getName(), boardAdd);
         Long addBoardId = boardService.add(boardAdd, authentication.getName());
 
-        return addBoardId + "번의 게시글 등록이 완료 되었습니다.";
+        return "게시글 등록이 완료 되었습니다.";
     }
 
     @GetMapping("/boards")
@@ -54,5 +54,20 @@ public class BoardApiController {
         BoardLookupInfo boardInfo = boardService.findBoard(boardId);
 
         return boardInfo;
+    }
+
+    @DeleteMapping
+    public String removeBoard(@RequestBody BoardRemoveInfo removeInfo, Authentication authentication) {
+        String writerId = removeInfo.getWriterId(); // 게시글 작성자 ID
+        String loginId = authentication.getName();  // 로그인한 ID
+        String boardId = removeInfo.getBoardId();
+        // 로그인 ID와 게시글 작성자 ID가 다른 경우 오류 (삭제 불가)
+        if (!writerId.equals(loginId)) {
+            throw new IllegalArgumentException("삭제에 실패했습니다. 해당 게시글은 " + writerId + " 님이 삭제할 수 있습니다.");
+        }
+        // 게시글 삭제
+        boardService.removeBoard(boardId);
+
+        return "게시글이 삭제 되었습니다.";
     }
 }
